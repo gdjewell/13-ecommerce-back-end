39dad59e6cabf70d93ec89e219b87ed1a15eba3a
gdjewell <gdjewell@gmail.com>
2023-02-07 19:14:34 -0500

    Initial commit

---
+++ routes/api/category-routes.js
@@ -0,0 +1,63 @@
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
+
+// The `/api/categories` endpoint
+
+router.get("/", (req, res) => {
+  Category.findAll({
+    include: {
+      model: Product,
+    },
+  }).then((data) => res.json(data));
+});
+
+router.get("/:id", (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  Category.findOne({
+    // Gets the book based on the isbn given in the request parameters
+    where: {
+      id: req.params.id,
+    },
+    include: {
+      model: Product,
+    },
+  }).then((data) => {
+    res.json(data);
+  });
+});
+
+router.post("/", (req, res) => {
+  // create a new category
+  console.log(req.body.category_name);
+  Category.create({
+    category_name: req.body.category_name,
+  }).then((data) => {
+    res.json(data);
+  });
+});
+
+router.put("/:id", (req, res) => {
+  // update a category by its `id` value
+  Category.update({
+    where: {
+      id: req.params.id,
+    },
+  }).then((data) => {
+    res.json(data);
+  });
+});
+
+router.delete("/:id", (req, res) => {
+  // delete a category by its `id` value
+
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  }).then((data) => {
+    res.json(data);
+  });
+});
+
+module.exports = router;
